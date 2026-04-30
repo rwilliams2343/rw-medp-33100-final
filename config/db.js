f2089f6211aabcc2194ec69b2e4ede903a3ceda4
rwilliams2343 <rochelle.williams80@myhunter.cuny.edu>
2026-04-30 14:04:00 -0400

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://lapislazu2343:ZDvjHzhtDAqivQWk@cluster0.fzwjh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = process.env.MONGODB_URI;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
